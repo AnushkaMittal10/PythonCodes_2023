8eac1c01e7cb2a82efcdcab707fe7ea77bcae1da
Anushka Mittal <anushkamittal0@gmail.com>
2023-10-17 19:12:54 +0530

    This is my code for checking duplicates in numpy array

---
+++ checking_duplicates.py
@@ -0,0 +1,11 @@
+import numpy as np
+enter_list = input("Enter your list as (1,2,x...):").split(",")
+map_int = map(int, enter_list)
+converted_map= list(map_int)
+ar1 = np.array(converted_map, dtype = int)
+print("The array is:", ar1)
+new_ar = np.array([], dtype = int)
+for i in ar1:
+    if i not in new_ar:
+        new_ar = np.append(new_ar, [i])
+print(new_ar)
