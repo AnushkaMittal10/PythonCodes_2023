81a858ae50d4e393d555385b8659d666c1375b0d
Anushka Mittal <anushkamittal0@gmail.com>
2023-10-18 18:13:39 +0530

    Updating a change

--- merging_dictonary.py
+++ merging_dictonary.py
@@ -1,5 +1,11 @@
 
+#Merging dictonaries using update function
 dict1 = {'name':"anushka", 'age':24}
 dict2 = {'location':"haldwani", 'gender':"female"}
 dict1.update(dict2)
 print(dict1)
+
+#Merging the dictonaries using a pipe operator 
+dict1 = {'name':"anushka", 'surname':"mittal"}
+dict2 = {'email': "anushkamittal0@gmail.com", 'gender':"female"}
+print(dict1|dict2)
