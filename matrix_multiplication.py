4ed3e2e801cfb2b5bd2dcea17bfea5089f190e02
Anushka Mittal <anushkamittal0@gmail.com>
2023-10-25 19:52:34 +0530

    Adding a code using numpy to multiply two matrices

---
+++ matrix_multiplication.py
@@ -0,0 +1,7 @@
+import numpy as np
+var = np.array([[1,2,3], [4,5,6]])
+var1 = np.array([[5,6,7], [9,10,11]])
+mul = var*var1
+print("First Matrix:\n", var)
+print("Second Matrix:\n", var1)
+print("The multiplication of two matrix:\n", mul)
